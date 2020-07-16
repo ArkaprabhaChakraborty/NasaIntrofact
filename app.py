7fe0e52f0d1d43b05e8a79969a7598285661445f
Arkaprabha Chakraborty <chakrabortyarkaprabha998@gmail.com>
2020-07-17 01:18:46 +0530

    Changes to be committed: 	new file:   app.py 	new file:   templates/layout.html

---
+++ app.py
@@ -0,0 +1,17 @@
+import json
+import requests
+from flask import Flask, render_template, url_for,redirect 
+
+key = "IlBg3J5cz9Lph2BYmwOb6WUmCi3lEtPAfb8Xf24V"
+
+rev = requests.get("https://api.nasa.gov/planetary/apod?api_key="+key)
+rev = rev.json()
+image = rev['hdurl']
+ex = rev['explanation']
+print(rev)
+app = Flask(__name__)
+@app.route("/",methods = ['GET','POST'])
+def page():
+    return render_template("layout.html", image = image,ex = ex)
+if __name__=="__main__":
+    app.run(debug = True)
